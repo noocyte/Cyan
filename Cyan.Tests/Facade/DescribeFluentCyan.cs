fe4873fe979fbf296795a230cc4471e9b107c228
noocyte <jarle.nygaard@outlook.com>
2013-11-07 10:50:20 +0100

    Beginning to add fluent interface

---
+++ Cyan.Tests/Facade/DescribeFluentCyan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyan.Tests.Facade
+{
+    public class DescribeFluentCyan
+    {
+        [Test]
+    }
+}
