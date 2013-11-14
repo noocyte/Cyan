4ce5c0672f438e56db0abefac36fb47294faa1c0
Parajao <alex.digioia@gmail.com>
2013-11-14 12:16:53 +0100

    using optimizations

--- Cyan.Tests/Facade/DescribeFluentCyan.cs
+++ Cyan.Tests/Facade/DescribeFluentCyan.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Runtime.Remoting.Contexts;
 using System.Threading.Tasks;
 using Cyan.Fluent;
 using Cyan.Tests.Helpers;
